5d477ee2ae031f6443777da9eec2e4087c4fa076
x-frank23-x <xfranklyngarzonx@gmail.com>
2026-09-02 01:35:52 -0500

    solucion temporal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Libro from "./Screens/Libro";
 
 function App() {
   return (
-    <BrowserRouter basename="/mapachesito">
-      <Routes>
-        <Route path="/" element={<Libro />} />
-      </Routes>
-    </BrowserRouter>
+    <Libro/>
   );
 }
 
