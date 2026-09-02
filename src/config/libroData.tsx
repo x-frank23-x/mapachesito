d66ff0a640ee20e2d4746e74f325fb507d3d049f
x-frank23-x <xfranklyngarzonx@gmail.com>
2026-09-02 01:42:05 -0500

    solucion temporal2

--- src/config/libroData.tsx
+++ src/config/libroData.tsx
@@ -3,22 +3,21 @@ import { motion } from "framer-motion";
 import { ListaCitas } from "../Components/CitasComponents";
 
 // ==========================================
-// 1. IMPORTACIÓN DIRECTA DE TODAS LAS IMÁGENES
+// IMPORTACIÓN DIRECTA DE IMÁGENES DESDE src/assets/
 // ==========================================
-import imgPortada from "../assets/portada.jpeg";
-import img1 from "../assets/1.jpeg";
-import img2 from "../assets/2.jpeg";
-import img3 from "../assets/3.jpeg";
-import img4 from "../assets/4.jpeg";
-import img5 from "../assets/5.jpeg";
-import img6 from "../assets/6.jpeg";
-import img7 from "../assets/7.jpeg";
-import img8 from "../assets/8.jpeg";
-import img9 from "../assets/9.jpeg";
-import img10 from "../assets/10.jpeg";
-import img11 from "../assets/11.jpeg";
-import img12 from "../assets/12.jpeg";
-
+import imgPortada from "../../assets/portada.jpeg";
+import img1 from "../../assets/1.jpeg";
+import img2 from "../../assets/2.jpeg";
+import img3 from "../../assets/3.jpeg";
+import img4 from "../../assets/4.jpeg";
+import img5 from "../../assets/5.jpeg";
+import img6 from "../../assets/6.jpeg";
+import img7 from "../../assets/7.jpeg";
+import img8 from "../../assets/8.jpeg";
+import img9 from "../../assets/9.jpeg";
+import img10 from "../../assets/10.jpeg";
+import img11 from "../../assets/11.jpeg";
+import img12 from "../../assets/12.jpeg";
 export interface HojaData {
   id: string | number;
   esTapa: boolean;
